21656947e4d96114033d68f5763248df7f01c422
shubhamKumar <appscicseshubham22@gmail.com>
2024-07-15 12:59:25 +0530

    minar

--- app/src/main/java/com/example/student_project/SecondActivity.kt
+++ app/src/main/java/com/example/student_project/SecondActivity.kt
@@ -1,12 +1,18 @@
 package com.example.student_project
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SecondActivity : AppCompatActivity() {
+
+    var btn: Button?=null//var declaration
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,10 @@ class SecondActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        btn=findViewById(R.id.button4)
+        val Intent= Intent(this,MainActivity::class.java)
+        startActivity(Intent)
+
     }
 }
